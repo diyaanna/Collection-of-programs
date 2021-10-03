ba184a61c1ace82edfe6ebd70cf2d55e2b91ea4e
narounsoul <83201355+naronsoul55@users.noreply.github.com>
2021-10-03 21:56:33 +0530

    kmtomiles.py

---
+++ kmtomiles.py
@@ -0,0 +1,4 @@
+kilometre_1 = float (input ("Please enter the speed in Kilometre: "))  
+conversion_ratio_1 = 0.621371  
+miles_1 = kilometre_1 * conversion_ratio_1  
+print ("The speed value in Miles: ", miles_1)  
